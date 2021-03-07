769296e19e4091dc659a13156e8dad6a9777d9c8
Evan <38842400+CodeSofty@users.noreply.github.com>
2021-03-07 13:26:02 -0600

    Update database.php

--- model/database.php
+++ model/database.php
@@ -13,16 +13,34 @@
     }
 
 
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+// //Get Heroku ClearDB connection information
+// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+// $cleardb_server = $cleardb_url["host"];
+// $cleardb_username = $cleardb_url["user"];
+// $cleardb_password = $cleardb_url["pass"];
+// $cleardb_db = substr($cleardb_url["path"],1);
+// $active_group = 'default';
+// $query_builder = TRUE;
+// // Connect to DB
+// $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
 
 
 ?>
+
+
+
+//HEROKU DATABASE CONNECTION
+    
+    $username = 'o5zwd89th0bp1tlx';
+    $password = 'p16f6agy45od7l4l';
+
+
+    try {
+        $db = new PDO('mysql:host=pxukqohrckdfo4ty.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=imes85fmrw4t7cfi', $username, $password);
+    } catch (PDOException $e) {
+        $error_message = $e->getMessage();
+        echo $error_message;
+        exit();
+    }
